1c026c64bda5a79e3b607306b3db9dc0caaf131e
Max Kharandziuk <max.kharandziuk@gmail.com>
2020-03-06 13:22:19 +0100

    added basic docker-compose

---
+++ backend/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+python manage.py migrate
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')"
+python manage.py runserver 0.0.0.0:8000
